7a97beff8e58fede79298cb41ac1ec10b555dd80
ptormay <peter.tormay@iddknowledge.com>
2015-07-26 15:11:37 +0100

    added testfunction

---
+++ R/df.R
@@ -0,0 +1,25 @@
+#' Return the dataframe 
+#' 
+#' Takes in a data frame or csv file consisting of four columns:
+#'  - Patient Key
+#'  - category
+#'  - event parameter (censoring)
+#'  - time
+#'  @param KMdata a data frame for calculating survival curve
+#'  @return a data frame containing the result of the survival calculation
+#'  @export
+df <- function(KMdata){
+  
+  # load necessary libraries
+  library(survival)
+  
+  #input can either be csv file or data   
+  KMtest <- if(is.character(KMdata) && file.exists(KMdata)){ 
+    read.csv(KMdata) 
+  } else { 
+    as.data.frame(KMdata) 
+  } 
+  
+return (KMtest)
+
+}
